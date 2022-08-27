1a88a1f3a1e0f90f35e26c6a5d6ceea4e307cab8
Azul Orellanos <azul.orellanos@mercadolibre.com>
2022-08-27 00:24:04 -0300

    Act. JS-Creatucv

--- app.js
+++ app.js
@@ -5,13 +5,25 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3100, () => {
-    console.log('Servidor levantado en el puerto 3100');
-});
 
+// Se ontiene la vista del HOME 
 
 app.get('/', (req, res) => {
 
     res.sendFile(path.join(__dirname, '/views/home.html'))
 
-})
+});
+
+// Se obtiene la vista de CREA TU CV
+
+app.get('/creatucv', (req, res) => {
+
+    res.sendFile(path.join(__dirname, '/views/creatucv.html'))
+});
+
+
+
+
+app.listen(3100, () => {
+    console.log('Servidor levantado en el puerto 3100');
+});
