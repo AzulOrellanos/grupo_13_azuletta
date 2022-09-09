8de1b8471324cd2ba349011fb53b41ba22965676
matirene <matirene1@hotmail.com>
2022-09-08 23:39:11 -0300

    Implentacion EJS

--- app.js
+++ app.js
@@ -3,62 +3,61 @@ const path = require('path');
 
 const app = express();
 
+// Indicamos a Express el template engine EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Indicamos la carpeta de recursos estaticos
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Se onbtienen la vistas
+
+app.get('/', (req, res) => {
+
+    res.render('home')
+});
+
+
+app.get('/creatucv', (req, res) => {
+
+    res.render('creatucv')
+});
+
+
+app.get('/login', (req, res) => {
+
+    res.render('login')
+});
+
+
+app.get('/registrarse', (req, res) => {
+
+    res.render('registrarse')
+});
+
+
+app.get('/carrito', (req, res) => {
+
+    res.render('carrito')
+});
+
+
+app.get('/sobrenosotros', (req, res) => {
+
+    res.render('sobrenosotros')
+});
+
+
+app.get('/contacto', (req, res) => {
+
+    res.render('contacto')
+});
+
 
 // Se levanta el servidor
 
 app.listen(3100, () => {
     console.log('Servidor levantado');
     console.log('http://localhost:3100');
-});
-
-
-// Se ontiene la vista del HOME 
-
-app.get('/', (req, res) => {
-
-    res.sendFile(path.join(__dirname, '/views/home.html'))
-
-});
-
-// Se obtiene la vista de CREA TU CV
-
-app.get('/creatucv', (req, res) => {
-
-    res.sendFile(path.join(__dirname, '/views/creatucv.html'))
-});
-
-// Se obtiene la vista de LOGIN
-
-app.get('/login', (req, res) => {
-
-    res.sendFile(path.join(__dirname, '/views/login.html'))
-});
-
-// Se obtiene la vista de REGISTRARSE
-
-app.get('/registrarse', (req, res) => {
-
-    res.sendFile(path.join(__dirname, '/views/registrarse.html'))
-});
-
-// Se obtiene la vista del CARRITO
-
-app.get('/carrito', (req, res) => {
-
-    res.sendFile(path.join(__dirname, '/views/carrito.html'))
-});
-
-// Se obtiene la vista de SOBRE NOSOTROS
-
-app.get('/sobrenosotros', (req, res) => {
-
-    res.sendFile(path.join(__dirname, '/views/sobrenosotros.html'))
-});
-
-// Se obtiene la vista de CONTACTO
-
-app.get('/contacto', (req, res) => {
-
-    res.sendFile(path.join(__dirname, '/views/contacto.html'))
 });
