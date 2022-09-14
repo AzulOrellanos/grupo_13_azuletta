fe5fc25f20f4b200b5399bbee1fb53e92638946b
matirene <matirene1@hotmail.com>
2022-09-13 23:34:26 -0300

    Actualizacion productDetail

--- src/controllers/curriculums.js
+++ src/controllers/curriculums.js
@@ -4,36 +4,39 @@ const curriculums = [
         nombre: 'CV Silver',
         descripcion: 'Diseño clásico',
         precio: '$500',
-        imagen: '/images/CVSilver.png',
+        logo: '/images/CVSilver.png',
         funciones: ['Crear 1 curriculum.', 'Acesso a todas las plantillas silver.',
         'Asesoria de profesionales.', 'Entre 24-48 horas disponible para descargarlo desde su cuenta personal.',
-        'Cuenta Azuletta durante 3 meses para crear y editar sin limites.'
-        ]
+        'Cuenta Azuletta durante 3 meses para crear y editar sin limites.'],
+        estilos: ['/images/cv1.jpg', '/images/cv2.png', '/images/cv3.jpg', '/images/cv4.png', '/images/cv5.jpg', 
+        '/images/cv6.jpg', '/images/cv7.jpg', '/images/cv8.jpg', '/images/cv9.jpg']
     },
     {
         id: 2,
         nombre: 'CV Gold',
         descripcion: 'Diseño innovador',
         precio: '$1000',
-        imagen: '/images/CVGold.png',
+        logo: '/images/CVGold.png',
         funciones: ['Crear 1 curriculum.', 'Acesso a todas las plantillas gold.',
         'Asesoria de profesionales.', 'Ofertas de trabajos coincidentes.', 
         'Entre 24-48 horas disponible para descargarlo desde su cuenta personal.',
-        'Cuenta Azuletta durante 3 meses para crear y editar sin limites.'
-        ]
+        'Cuenta Azuletta durante 3 meses para crear y editar sin limites.'],
+        estilos: ['/images/cv1.jpg', '/images/cv2.png', '/images/cv3.jpg', '/images/cv4.png', '/images/cv5.jpg', 
+        '/images/cv6.jpg', '/images/cv7.jpg', '/images/cv8.jpg', '/images/cv9.jpg']
     },
     {
         id: 3,
         nombre: 'CV Black',
         descripcion: 'Diseño de alto impacto',
         precio: '$1500',
-        imagen: '/images/CVBlack.png',
+        logo: '/images/CVBlack.png',
         funciones: ['Crear 1 curriculum.', 'Acesso a todas las plantillas black.',
         'Asesoria de profesionales.', 'Ofertas de trabajos coincidentes.',
         'Seguimiento de solicitudes de empleo.', 'Incluye carta de presentación personalizada.',
         'Entre 24-48 horas disponible para descargarlo desde su cuenta personal.',
-        'Cuenta Azuletta durante 3 meses para crear y editar sin limites.'
-        ]
+        'Cuenta Azuletta durante 3 meses para crear y editar sin limites.'],
+        estilos: ['/images/cv1.jpg', '/images/cv2.png', '/images/cv3.jpg', '/images/cv4.png', '/images/cv5.jpg', 
+        '/images/cv6.jpg', '/images/cv7.jpg', '/images/cv8.jpg', '/images/cv9.jpg']
     }
 
 ]
